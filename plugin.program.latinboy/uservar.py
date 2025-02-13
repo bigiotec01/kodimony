e528890157b9703c745b4aff8f52b73f6a2f1818
carlosGT7274 <carlosbt7274@gmail.com>
2025-02-13 17:12:24 -0600

    commit inicial

---
+++ plugin.program.latinboy/uservar.py
@@ -0,0 +1,10 @@
+import xbmcaddon
+
+'''#####-----Build File-----#####'''
+buildfile = 'http://monykodi.rf.gd/builds/builds.json'
+
+'''#####-----Notifications File-----#####'''
+notify_url  = 'https://raw.githubusercontent.com/latinboykodi/latinboykodi.github.io/main/builds/notify.txt'
+
+'''#####-----Excludes-----#####'''
+excludes  = ['plugin.video.whatever']
