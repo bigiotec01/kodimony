f59bec556bd61c8b385303894d4743102004edbf
bigiotec01 <118586427+bigiotec01@users.noreply.github.com>
2026-08-02 12:18:53 -0400

    Point buildfile/build URLs at bigiotec01/kodimony
    The repo was transferred/renamed from CarlosGT7274/kodimony to
    bigiotec01/kodimony. raw.githubusercontent.com does not reliably follow
    renames the way github.com does, causing intermittent 404s on the
    buildfile URL (seen live: build_menu() crashed with
    urllib.error.HTTPError 404 while fetching builds.json).

--- plugin.program.latinboy/uservar.py
+++ plugin.program.latinboy/uservar.py
@@ -1,7 +1,7 @@
 import xbmcaddon
 
 '''#####-----Build File-----#####'''
-buildfile = 'https://raw.githubusercontent.com/CarlosGT7274/kodimony/refs/heads/main/builds/builds.json'
+buildfile = 'https://raw.githubusercontent.com/bigiotec01/kodimony/refs/heads/main/builds/builds.json'
 
 '''#####-----Notifications File-----#####'''
 notify_url  = 'https://raw.githubusercontent.com/latinboykodi/latinboykodi.github.io/main/builds/notify.txt'
